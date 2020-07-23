fe4cc9debaedb1963ca6bc8444bc52dc214d7112
0scarecrow0 <1432686114@qq.com>
2020-07-23 23:14:08 +0800

    测试分支上传

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-const yargs = require('yargs')
-const Server = require('./app')
+const yargs = require("yargs");
+const Server = require("./app");
+// 我修改了昂
+const argv = yargs
+  .usage("xiaobai01 [options]")
+  .option("p", {
+    alias: "port", // 别名
+    describe: "端口号", // 描述
+    default: "7899", // 默认值
+  })
+  .option("h", {
+    alias: "hostname", // 别名
+    describe: "host", // 描述
+    default: "127.0.0.1", // 默认值
+  })
+  .option("d", {
+    alias: "root", // 别名
+    describe: "root path", // 描述
+    default: process.cwd(), // 默认值
+  })
+  .version()
+  .alias("v", "version")
+  .help().argv;
 
+const server = new Server(argv);
 
-const argv = yargs.usage('xiaobai01 [options]')
-    .option('p',{
-        alias:'port',       // 别名
-        describe:'端口号',  // 描述
-        default:'7899'     // 默认值
-    })
-    .option('h',{
-        alias:'hostname',       // 别名
-        describe:'host',  // 描述
-        default:'127.0.0.1'     // 默认值
-    })
-    .option('d',{
-        alias:'root',       // 别名
-        describe:'root path',  // 描述
-        default:process.cwd()     // 默认值
-    })
-    .version()
-    .alias('v','version')
-    .help()
-    .argv
-
-const server = new Server(argv)
-
-server.start()
+server.start();
