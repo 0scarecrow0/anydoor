dac3c8ce9aa538e8e6e2f35cdb40d83193b368c5
0scarecrow0 <1432686114@qq.com>
2020-07-23 23:26:18 +0800

    测试分支

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,27 @@
 module.exports = {
-    'root':true,
-	'env': {
-		'commonjs': true,
-		'es6': true,
-		'node': true
-	},
-    'extends': 'eslint:recommended',
-	'globals': {
-    },
-    'parser':'babel-eslint',
-	'parserOptions': {
-		'ecmaVersion': 2018    
-	},
-	'rules': {
-		'indent': [
-			'error',
-			4
-		],
-		'linebreak-style': [
-			2,
-			'unix'
-		],
-		'quotes': [
-			2,
-			'single'
-        ],
-        "no-console":["error",{ // 不允许写console
-                "allow":["warn","error","info","log"]
-                // 但是可以写console.warn,error,info
-            }
-        ]
-	}
+  root: true,
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  globals: {},
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    indent: ["error", 4],
+    "linebreak-style": [2, "unix"],
+    quotes: [2, "single"],
+    "no-console": [
+      "error",
+      {
+        // 不允许写console
+        allow: ["warn", "error", "info", "log"],
+        // 但是可以写console.warn,error,info
+      },
+    ],
+  },
 };
